ddfb1f79ae25b841e1e98b8319ea1ae7057d8ed8
Jonel-Code <akoilypants3@gmail.com>
2019-07-04 19:22:16 +0800

    modify dbConnection module to handle large pool size

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,25 +2,29 @@
 
 const db_config = require('./database_config.json');
 const mysql = require('mysql');
-const connection = mysql.createConnection(db_config);
+const connection = mysql.createPool({ ...db_config, connectionLimit: 100 });
+
+const getConnection = function (callback){
+	connection.getConnection(function (err, connection){
+		callback(err, connection);
+	});
+};
 
 module.exports = class DbConnection {
 	static execute (sql = '', callback = (err, rows, fields) => {}, params = []) {
-		connection.connect((err) => {
-			if (!err) {
-				console.log('connection established');
-			}
-			else {
-				console.log(err);
+		getConnection((error, connection) => {
+			try {
+				console.log('execute query: ', sql);
+				connection.query(sql, params, callback);
+			} catch (err) {
+				throw 'Error in Query\n';
+			} finally {
+				if (error) {
+					console.log('error', error);
+				}
+				// connection.end();
+				connection.release();
 			}
 		});
-		try {
-			console.log('execute query: ', sql);
-			connection.query(sql, params, callback);
-		} catch (err) {
-			throw 'Error in Query\n';
-		} finally {
-			connection.end();
-		}
 	}
 };
