f046788855b010e608ff5ad6b02fcea0de5a3aef
Jonel-Code <akoilypants3@gmail.com>
2019-07-04 18:57:06 +0800

    implemented api boostrapper in app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const apiEndpoints = require('./apiEndpoints');
+apiEndpoints.boostrap(app);
+
 app.get('/', (req, res) => res.send('Welcome to express app'));
 
 app.listen(port, () => console.log(`Express app listening on port ${port}!`));
