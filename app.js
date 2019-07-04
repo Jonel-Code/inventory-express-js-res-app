663b7c84c00fcab3c8b7e267530633eb2d6558d5
Jonel-Code <akoilypants3@gmail.com>
2019-07-04 21:14:23 +0800

    added creation of table upon startup

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const items = require('./database/models/items');
+items.create_table();
+
 const apiEndpoints = require('./apiEndpoints');
 apiEndpoints.boostrap(app);
 
