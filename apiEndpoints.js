e999c177e9068ae5354480ee900f62d145ebaa46
Jonel-Code <akoilypants3@gmail.com>
2019-07-04 20:31:27 +0800

    added get api endpoint

--- apiEndpoints.js
+++ apiEndpoints.js
@@ -1,9 +1,25 @@
+const items = require('./database/models/items');
+
 const PREFIX = '/api/';
 function add_prefix (str = ''){
 	return PREFIX + str;
 }
+
+const get_inventory_data = function (req, res){
+	const params = req.query;
+	items
+		.find_item({ name: params.name, qty: params.qty, amount: params.amount, id: params.id })
+		.then((result = []) => {
+			res.send(
+				result.map((x) => {
+					return x.to_json;
+				}),
+			);
+		});
+};
+
 module.exports = class ApiEndpoints {
 	static boostrap (app) {
-		app.get(add_prefix('home'), (req, res) => res.send('Welcome to express app'));
+		app.get(add_prefix('inventory'), get_inventory_data);
 	}
 };
